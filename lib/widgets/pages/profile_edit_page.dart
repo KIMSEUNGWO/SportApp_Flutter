db32347f4d66b07ff469455d4957327296791b0e
김승우 <tmd8633@naver.com>
2024-07-17 23:47:56 +0900

    feat : 프로필 수정페이지 키보드 대응 완료

--- lib/widgets/pages/profile_edit_page.dart
+++ lib/widgets/pages/profile_edit_page.dart
@@ -128,6 +128,7 @@ class _ProfileEditPageState extends ConsumerState<ProfileEditPage> {
       appBar: AppBar(
         centerTitle: false,
         title: Text('profile').tr(gender: 'editProfile'),
+        scrolledUnderElevation: 0,
         actions: [
           GestureDetector(
             onTap: () => editProfile(context),
@@ -143,206 +144,214 @@ class _ProfileEditPageState extends ConsumerState<ProfileEditPage> {
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        child: GestureDetector(
-          onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
-          child: Container(
-            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-            decoration: BoxDecoration(),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    selectImage();
-                  },
-                  child: Stack(
-                      children: [
-                        (editImage == null)
-                            ? EmptyProfileImage()
-                            : Container(
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+
+          final keyboardHeight = MediaQuery.of(context).viewInsets.bottom;
+
+          return SingleChildScrollView(
+            child: GestureDetector(
+              onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+              child: Container(
+                margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                padding: EdgeInsets.only(bottom: keyboardHeight),
+                decoration: const BoxDecoration(),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        selectImage();
+                      },
+                      child: Stack(
+                          children: [
+                            (editImage == null)
+                                ? EmptyProfileImage()
+                                : Container(
                                 width: 100, height: 100,
                                 decoration: BoxDecoration(
                                   borderRadius: BorderRadius.circular(100),
                                 ),
                                 clipBehavior: Clip.hardEdge,
                                 child: editImage
+                            ),
+                            Positioned(
+                              bottom: 0, right: 0,
+                              child: Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(100),
+                                  color: Colors.black,
+                                ),
+                                child: Icon(Icons.camera_alt,
+                                  color: Colors.white,
+                                  size: 20,
+                                ),
                               ),
-                        Positioned(
-                          bottom: 0, right: 0,
-                          child: Container(
-                            padding: EdgeInsets.all(10),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(100),
-                              color: Colors.black,
                             ),
-                            child: Icon(Icons.camera_alt,
-                              color: Colors.white,
-                              size: 20,
-                            ),
+                          ]
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Text('user',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ).tr(gender: 'nickname'),
+                    TextField(
+                      controller: _textNicknameController,
+                      style: TextStyle(
+                        fontSize: 21,
+                      ),
+                      decoration: InputDecoration(
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xFFE4E2E2), width: 1.5),
                           ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xFFE4E2E2), width: 1.5),
+                          ),
+                          hintText: ref.read(loginProvider.notifier).state!.name
+                      ),
+
+                    ),
+                    SizedBox(height: 30,),
+                    Text('user',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ).tr(gender: 'introduce'),
+                    TextFormField(
+                      controller: _textIntroController,
+
+                      keyboardType: TextInputType.multiline,
+                      onChanged: (text) => validLengthText(text),
+                      maxLines: null,
+                      style: const TextStyle(fontSize: 17),
+                      decoration: InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFFE4E2E2), width: 1.5),
                         ),
-                      ]
-                  ),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Text('user',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ).tr(gender: 'nickname'),
-                TextField(
-                  controller: _textNicknameController,
-                  style: TextStyle(
-                    fontSize: 21,
-                  ),
-                  decoration: InputDecoration(
-                      enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Color(0xFFE4E2E2), width: 1.5),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFFE4E2E2), width: 1.5),
+                        ),
+                        hintText: ref.read(loginProvider.notifier).state!.intro ?? '자기소개를 적어주세요.',
+                        counterText: '$introCurrentCount / $introMaxCount',
+                        counterStyle: const TextStyle(fontSize: 14),
                       ),
-                      focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Color(0xFFE4E2E2), width: 1.5),
-                      ),
-                      hintText: ref.read(loginProvider.notifier).state!.name
-                  ),
-
-                ),
-                SizedBox(height: 30,),
-                Text('user',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ).tr(gender: 'introduce'),
-                TextFormField(
-                  controller: _textIntroController,
-
-                  keyboardType: TextInputType.multiline,
-                  onChanged: (text) => validLengthText(text),
-                  maxLines: null,
-                  style: const TextStyle(fontSize: 17),
-                  decoration: InputDecoration(
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xFFE4E2E2), width: 1.5),
                     ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xFFE4E2E2), width: 1.5),
+
+                    SizedBox(
+                      height: 10,
                     ),
-                    hintText: ref.read(loginProvider.notifier).state!.intro ?? '자기소개를 적어주세요.',
-                    counterText: '$introCurrentCount / $introMaxCount',
-                    counterStyle: const TextStyle(fontSize: 14),
-                  ),
-                ),
 
-                SizedBox(
-                  height: 10,
-                ),
-
-                Row(
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    Row(
                       children: [
-                        Text('user',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ).tr(gender: 'sex'),
-                        SizedBox(height: 15,),
-                        Container(
-                          width: 150,
-                          height: 55,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            // color: Color(0xFFE9F1FA),
-                            color: Color(0xFFE6E6E6),
-                          ),
-                          child: Flex(
-                            direction: Axis.horizontal,
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('user',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ).tr(gender: 'sex'),
+                            SizedBox(height: 15,),
+                            Container(
+                              width: 150,
+                              height: 55,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                // color: Color(0xFFE9F1FA),
+                                color: Color(0xFFE6E6E6),
+                              ),
+                              child: Flex(
+                                direction: Axis.horizontal,
+                                children: [
+                                  Expanded(
+                                    flex: 1,
+                                    child: Container(
+                                      margin: EdgeInsets.all(5),
+                                      decoration: (ref.read(loginProvider.notifier).state!.sex == 'F') ? myBoxDecoration : youBoxDecoration ,
+                                      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                                      child: Center(
+                                        child: Text('female',
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w500,
+                                            color: (ref.read(loginProvider.notifier).state!.sex == 'F') ? myTextColor : youTextColor,
+                                          ),
+                                        ).tr(),
+                                      ),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    flex: 1,
+                                    child: Container(
+                                      margin: EdgeInsets.all(5),
+                                      decoration: (ref.read(loginProvider.notifier).state!.sex == 'M') ? myBoxDecoration : youBoxDecoration,
+                                      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                                      child: Center(
+                                          child: Text('male',
+                                            style: TextStyle(
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.w500,
+                                              color: (ref.read(loginProvider.notifier).state!.sex == 'M') ? myTextColor : youTextColor,
+                                            ),
+                                          ).tr()
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(width: 15,),
+
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Expanded(
-                                flex: 1,
-                                child: Container(
-                                  margin: EdgeInsets.all(5),
-                                  decoration: (ref.read(loginProvider.notifier).state!.sex == 'F') ? myBoxDecoration : youBoxDecoration ,
-                                  padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                                  child: Center(
-                                    child: Text('female',
-                                      style: TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w500,
-                                        color: (ref.read(loginProvider.notifier).state!.sex == 'F') ? myTextColor : youTextColor,
-                                      ),
-                                    ).tr(),
+                              Text('user',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ).tr(gender: 'birthday'),
+                              SizedBox(height: 15,),
+                              Container(
+                                height: 55,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  // color: Color(0xFFE9F1FA),
+                                  color: Color(0xFFE6E6E6),
+                                ),
+                                child: Center(
+                                  child: Text('${ref.read(loginProvider.notifier).state!.birth}',
+                                    style: TextStyle(
+                                      color: Color(0xFF3E3E3E),
+                                      fontSize: 18,
+                                    ),
                                   ),
                                 ),
-                              ),
-                              Expanded(
-                                flex: 1,
-                                child: Container(
-                                  margin: EdgeInsets.all(5),
-                                  decoration: (ref.read(loginProvider.notifier).state!.sex == 'M') ? myBoxDecoration : youBoxDecoration,
-                                  padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                                  child: Center(
-                                    child: Text('male',
-                                      style: TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w500,
-                                        color: (ref.read(loginProvider.notifier).state!.sex == 'M') ? myTextColor : youTextColor,
-                                      ),
-                                    ).tr()
-                                  ),
-                                ),
-                              ),
+                              )
                             ],
                           ),
                         ),
-                      ],
-                    ),
-                    SizedBox(width: 15,),
 
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('user',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ).tr(gender: 'birthday'),
-                          SizedBox(height: 15,),
-                          Container(
-                            height: 55,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              // color: Color(0xFFE9F1FA),
-                              color: Color(0xFFE6E6E6),
-                            ),
-                            child: Center(
-                              child: Text('${ref.read(loginProvider.notifier).state!.birth}',
-                                style: TextStyle(
-                                  color: Color(0xFF3E3E3E),
-                                  fontSize: 18,
-                                ),
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
+                      ],
                     ),
 
                   ],
                 ),
-
-              ],
+              ),
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
